195dcce5118809f94a4ba69795e87370e77a3785
BrianPickens <brianpickens53@gmail.com>
2020-01-31 00:07:18 -0800

    adjusting camera adjusting

--- Assets/Scripts/DebugScripts/AdjustCameraHeight.cs
+++ Assets/Scripts/DebugScripts/AdjustCameraHeight.cs
@@ -9,6 +9,15 @@ public class AdjustCameraHeight : MonoBehaviour
     private Text CameraHeight = null;
 
     [SerializeField]
+    private Text CameraHorizontal = null;
+
+    [SerializeField]
+    private ToggleButton LeftButton = null;
+
+    [SerializeField]
+    private ToggleButton RightButton = null;
+
+    [SerializeField]
     private ToggleButton UpButton = null;
 
     [SerializeField]
@@ -27,6 +36,14 @@ public class AdjustCameraHeight : MonoBehaviour
         {
             DecreaseCameraHeight();
         }
+        else if (RightButton.GetLookStatus())
+        {
+            MoveRight();
+        }
+        else if (LeftButton.GetLookStatus())
+        {
+            MoveLeft();
+        }
 
         CameraHeight.text = Player.transform.position.y + "";
 
@@ -40,5 +57,17 @@ public class AdjustCameraHeight : MonoBehaviour
     public void DecreaseCameraHeight()
     {
         Player.transform.Translate(Vector3.down * Time.deltaTime * .5f);
+    }
+
+    public void MoveRight()
+    {
+        Vector3 moveVector = new Vector3(0, 0, 1);
+        Player.transform.Translate(moveVector * Time.deltaTime * .5f);
+    }
+
+    public void MoveLeft()
+    {
+        Vector3 moveVector = new Vector3(0, 0, -1);
+        Player.transform.Translate(moveVector * Time.deltaTime * .5f);
     }
 }
