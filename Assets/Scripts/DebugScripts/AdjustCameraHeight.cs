dcefcc75c2f10cd6e33cc4fe9568930757cf48b3
BrianPickens <brianpickens53@gmail.com>
2020-01-30 23:30:40 -0800

    Adding Height Adjust for camera

---
+++ Assets/Scripts/DebugScripts/AdjustCameraHeight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustCameraHeight : MonoBehaviour
+{
+    [SerializeField]
+    private Text CameraHeight = null;
+
+    [SerializeField]
+    private ToggleButton UpButton = null;
+
+    [SerializeField]
+    private ToggleButton DownButton = null;
+
+    [SerializeField]
+    private Transform Player = null;
+
+    private void Update()
+    {
+        if (UpButton.GetLookStatus())
+        {
+            IncreaseCameraHeight();
+        }
+        else if (DownButton.GetLookStatus())
+        {
+            DecreaseCameraHeight();
+        }
+    }
+
+    public void IncreaseCameraHeight()
+    {
+        Player.transform.Translate(Vector3.up * Time.deltaTime * .5f);
+    }
+
+    public void DecreaseCameraHeight()
+    {
+        Player.transform.Translate(Vector3.down * Time.deltaTime * .5f);
+    }
+}
---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : LookTarget
+{
+
+
+    [SerializeField]
+    private Image FillImage = null;
+
+    private bool buttonPressed = false;
+
+    private bool isPressable = false;
+
+    public override void LookStarted()
+    {
+        base.LookStarted();
+    }
+
+    public override void LookEnded()
+    {
+        base.LookEnded();
+    }
+
+    protected override void StartLookResponse()
+    {
+        base.StartLookResponse();
+        FillImage.fillAmount = 1f;
+    }
+
+    protected override void EndLookResponse()
+    {
+        base.EndLookResponse();
+        FillImage.fillAmount = 0f;
+    }
+
+    public bool GetLookStatus()
+    {
+        return isLookedAt;
+    }
+
+}
