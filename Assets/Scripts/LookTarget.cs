9ea057d2478ce8a11493a14ea36cb5d0efa5493b
BrianPickens <brianpickens53@gmail.com>
2020-02-04 23:43:07 -0800

    working on intro ui sound

--- Assets/Scripts/LookTarget.cs
+++ Assets/Scripts/LookTarget.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 //require a Box Collider so that there is a target to hit
 [RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(AudioSource))]
 
 public class LookTarget : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip SoundClip = null;
+
+    [SerializeField]
+    private float MaxVolume = 1f;
+
+    [SerializeField]
+    private float FadeSpeed = 1f;
+
     protected bool isLookedAt = false;
+
+    protected AudioSource myAudioSource = null;
+
+    private IEnumerator soundRoutine = null;
+
+    private void Awake()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+        myAudioSource.volume = 0f;
+        myAudioSource.clip = SoundClip;
+        myAudioSource.Play();
+    }
 
     public virtual void LookStarted()
     {
@@ -29,12 +51,55 @@ public class LookTarget : MonoBehaviour
 
     protected virtual void StartLookResponse()
     {
+        //transfer all of this to look button, and make a separate sound response for toggle button because
+        //you need it to play differently
+        if (SoundClip != null)
+        {
+            if (!myAudioSource.isPlaying)
+            {
+                myAudioSource.Play();
+            }
 
+            if (soundRoutine != null)
+            {
+                StopCoroutine(soundRoutine);
+            }
+            soundRoutine = SoundRoutine(true);
+            StartCoroutine(soundRoutine);
+        }
     }
 
     protected virtual void EndLookResponse()
     {
+        if (SoundClip != null)
+        {
+            if (soundRoutine != null)
+            {
+                StopCoroutine(soundRoutine);
+            }
+            soundRoutine = SoundRoutine(false);
+            StartCoroutine(soundRoutine);
+        }
+    }
 
+    private IEnumerator SoundRoutine(bool _fadeIn)
+    {
+        if (_fadeIn)
+        {
+            while (Mathf.Abs(myAudioSource.volume - MaxVolume) > Mathf.Epsilon)
+            {
+                myAudioSource.volume = Mathf.MoveTowards(myAudioSource.volume, MaxVolume, FadeSpeed * Time.deltaTime);
+                yield return null;
+            }
+        }
+        else
+        {
+            while (Mathf.Abs(myAudioSource.volume - 0) > Mathf.Epsilon)
+            {
+                myAudioSource.volume = Mathf.MoveTowards(myAudioSource.volume, 0f, FadeSpeed * Time.deltaTime);
+                yield return null;
+            }
+        }
     }
 
 }
---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<AudioClip> SoundClips = new List<AudioClip>();
+
+    private AudioSource myAudioSource = null;
+
+    private void Awake()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+
+        if (myAudioSource == null)
+        {
+            Debug.LogError("Sound Player on object without an Audio Source");
+        }
+
+    }
+
+    public void PlaySoundClip(int clipNumber = 0, float volume = 1f)
+    {
+        myAudioSource.Stop();
+        myAudioSource.volume = volume;
+        myAudioSource.PlayOneShot(SoundClips[clipNumber]);
+    }
+
+    public void PlaySoundClipContinuous(int clipNumber = 0, float volume = 1f)
+    {
+        myAudioSource.Stop();
+        myAudioSource.volume = volume;
+        myAudioSource.clip = SoundClips[clipNumber];
+        myAudioSource.Play();
+    }
+
+
+}
