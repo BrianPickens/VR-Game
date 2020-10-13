d8e34868a2154f5302bba986fcc06cf5a222d17c
BrianPickens <brianpickens53@gmail.com>
2020-10-13 00:02:41 -0700

    addign lock animations

---
+++ Assets/Scripts/Pumpkin/LockRemove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockRemove : MonoBehaviour
+{
+    [SerializeField] private Rigidbody myRigidbody = null;
+
+    public void TossLock()
+    {
+        myRigidbody.isKinematic = false;
+        myRigidbody.useGravity = true;
+
+        myRigidbody.AddForce(Vector3.forward * 3f, ForceMode.Impulse);
+        myRigidbody.AddTorque(Vector3.back * 2f, ForceMode.Impulse);
+    }
+}
--- Assets/Scripts/Pumpkin/TriggeredObject.cs
+++ Assets/Scripts/Pumpkin/TriggeredObject.cs
@@ -6,12 +6,17 @@ public class TriggeredObject : MonoBehaviour
 {
 
     [Header("Object")]
-    [SerializeField] private bool turnOnObject;
-    [SerializeField] private GameObject myGameobject;
+    [SerializeField] private bool turnOnObject = false;
+    [SerializeField] private GameObject myGameobject = null;
 
     [Header("Light")]
-    [SerializeField] private bool startLightFlicker;
-    [SerializeField] private Lighting objectLight;
+    [SerializeField] private bool startLightFlicker = false;
+    [SerializeField] private Lighting objectLight = null;
+
+    [Header("Animator")]
+    [SerializeField] private bool playAnimation = false;
+    [SerializeField] private int animationNum = 0;
+    [SerializeField] private Animator myAnimator = null;
 
     public void TriggerResponse()
     {
@@ -23,6 +28,32 @@ public class TriggeredObject : MonoBehaviour
         if (startLightFlicker)
         {
             objectLight.StartLightFlicker();
+        }
+
+        if (playAnimation)
+        {
+            switch (animationNum)
+            {
+                case 1:
+                    myAnimator.SetTrigger("Event1");
+                    break;
+
+                case 2:
+                    myAnimator.SetTrigger("Event2");
+                    break;
+
+                case 3:
+                    myAnimator.SetTrigger("Event3");
+                    break;
+
+                case 4:
+                    myAnimator.SetTrigger("Event4");
+                    break;
+
+                default:
+                    myAnimator.SetTrigger("Event1");
+                    break;
+            }
         }
 
     }
